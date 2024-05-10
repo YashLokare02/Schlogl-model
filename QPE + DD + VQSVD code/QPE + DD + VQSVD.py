4ddc9cd0cdab36c7057ec72e08dd620c3233ad18
YashLokare02 <86669612+YashLokare02@users.noreply.github.com>
2024-05-10 11:36:06 -0400

    Update QPE + DD + VQSVD.py
    Python file containing code to implement the quantum (QPE + VQSVD) subroutine.

--- QPE + DD + VQSVD code/QPE + DD + VQSVD.py
+++ QPE + DD + VQSVD code/QPE + DD + VQSVD.py
@@ -11,6 +11,7 @@ from scipy.stats import skew, kurtosis
 from scipy.optimize import curve_fit
 from scipy.linalg import norm
 import matplotlib.pyplot as plt
+import seaborn as sns
 import math
 import itertools
 from collections import Counter
@@ -2022,3 +2023,12 @@ def generate_layout(U, num_precision_qubits, num_query_qubits, optimization_leve
 
     # Plot the QPE circuit layout on the IBMQ backend
     plot_circuit_layout(transpiled_circuit, backend, view = 'physical') # visualization of the physical qubit mapping; also possible is 'virtual'
+
+    ## The code below generates heatmap plots to visualize the sparsity of the Schlogl operator(s)
+    # Create axes for heatmap plot
+    fig, ax = plt.subplots()
+    ax2 = sns.heatmap(hermitian_matrix, linewidth = 0.5, cmap = 'seismic')
+    ax.set_axis_off()
+
+    # Show the plot
+    plt.show()
